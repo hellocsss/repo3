7b923cead55d7a317d4219964bc420cfb66c838b
yzj <981892951@qq.com>
2019-08-21 22:37:33 +0800

    提交需要的文件

---
+++ demo/src/com/itheima/javase/Hello.java
@@ -0,0 +1,18 @@
+package com.itheima.javase;
+
+public class Hello {
+    public static void main(String[] args) {
+        //f7 f8 f9
+        int a=1;
+        int b=a+2;
+        System.out.println("hello idea");
+        System.out.println("hello idea");
+        System.out.println("hello idea");
+        System.out.println("hello idea");
+        aa();
+        System.out.println("hello idea");
+    }
+    public static  void aa(){
+        System.out.println("hello idea.....");
+    }
+}
